05f168544618738657f50b3e2822bebb400eac3c
NathanBergmann <nathan.bergmann@agapys.com>
2023-11-07 20:20:58 -0300

    Adicionado Spring Boot

---
+++ back-end/controleDeEstoque/src/main/java/controleDeEstoque/ControleDeEstoqueApplication.java
@@ -0,0 +1,14 @@
+package controleDeEstoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeEstoqueApplication {
+
+	public static void main(String[] args) {
+		System.out.println("ola");
+		SpringApplication.run(ControleDeEstoqueApplication.class, args);
+	}
+
+}
